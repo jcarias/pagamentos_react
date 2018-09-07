bff1312635316de3afb80ec0235090fb56c4a0ea
João Carias <carias.joao@gmail.com>
2018-09-07 14:50:36 +0100

    Change README file

--- src/components/charts/ServicesYearChart.jsx
+++ src/components/charts/ServicesYearChart.jsx
@@ -22,11 +22,29 @@ const ServicesYearChart = props => {
     ];
 
     return (
-        <ResponsiveContainer width="100%" height={100}>
-            <BarChart data={data}>
+        <ResponsiveContainer width={700} height="80%">
+            <AreaChart
+                data={data}
+                margin={{ top: 20, right: 30, left: 0, bottom: 0 }}
+            >
                 <XAxis dataKey="name" />
-                <Bar dataKey="uv" fill="red" />
-            </BarChart>
+                <YAxis />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Tooltip />
+                <ReferenceLine x="Page C" stroke="green" label="Min PAGE" />
+                <ReferenceLine
+                    y={4000}
+                    label="Max"
+                    stroke="red"
+                    strokeDasharray="3 3"
+                />
+                <Area
+                    type="monotone"
+                    dataKey="uv"
+                    stroke="#8884d8"
+                    fill="#8884d8"
+                />
+            </AreaChart>
         </ResponsiveContainer>
     );
 };
